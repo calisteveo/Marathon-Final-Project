4faff315a3b0e3e5a17e72011743faa69c227211
calisteveo <soglesbee1@gmail.com>
2014-03-27 09:05:17 -0700

    added create to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,23 @@ class UsersController < ApplicationController
   def index
   end
 
-  # def show
-  #   @user = User.find(params[:id])
-  #   @journey = @user.journeys
-  #   render :show
-  # end
+  def show
+    @user = User.find(params[:id])
+    @journey = @user.journeys
+    render :show
+  end
 
-  # def new
-  #   @user = User.new()
-  # end
+  def new
+    @user = User.new()
+  end
+
+  def create
+    new_user = params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    @user=User.new(new_user)
+    if @user.save
+      sign_in @user
+      redirect_to '/'
+    else
+      render :new
+    end
 end
