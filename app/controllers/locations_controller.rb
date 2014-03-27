2717ee614eff62f692b9edb40a345fb25998fa67
calisteveo <soglesbee1@gmail.com>
2014-03-27 08:23:09 -0700

    added functionality to locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,21 @@
 class LocationsController < ApplicationController
   def new
+    @location = Location.new
   end
 
   def create
+    new_journey = params.require(:location).permit(:name, :origin, :orig_lat, :orig_lng, :destination, :dest_lat, :dest_lng, :user_id)
+    @location = Location.create(new_location)
+    render :show
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def destroy
+    location = Location.find(params[:id])
+    location.delete
+    redirect_to(locations_path)
   end
 end
