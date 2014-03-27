2b8bd6ca98931ef0bd80fc9004ec5bc57e944002
calisteveo <soglesbee1@gmail.com>
2014-03-27 09:19:10 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ActiveRecord::Base
 
   has_many :locations
   has_many :journeys
+
+  has_secure_password
+
+  before_save { |user| user.username = username.downcase }
+  before_save :create_remember_token
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
   
 end
