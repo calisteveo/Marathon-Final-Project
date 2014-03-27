891beb7be51168e7f80f22cec4ddc3afd9ccfbbd
calisteveo <soglesbee1@gmail.com>
2014-03-27 09:00:03 -0700

    added create and delete to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user=User.find_by_username(params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to '/'
+    else
+      #flash error message and render sign-in form
+      flash.now[:error]='Invalid username/password combination'
+      render :new
+    end
   end
 
   def destroy
+    sign_out
+    redirect_to '/'
   end
 end
