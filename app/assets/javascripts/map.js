d5886ca9cc16c2659299710908c4418cd6e2da55
calisteveo <soglesbee1@gmail.com>
2014-03-28 09:23:14 -0700

    map renders with current location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,16 +1,38 @@
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 var map;
+var haight = new google.maps.LatLng(37.7699298, -122.4469157);
+var oceanBeach = new google.maps.LatLng(37.7683909618184, -122.51089453697205);
 var markers =[];
+var route = [];
 
 function initialize(){
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
 
   var mapOptions = {
     center: new google.maps.LatLng(37.7933, -122.3167),
     zoom: 12
   };
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+  function calcRoute() {
+    var start = document.getElementById("start").value;
+    var end = document.getElementById("end").value;
+    var request = {
+      origin: haight,
+      destination: oceanBeach,
+    };
+    directionsService.route(request, function(response, status){
+      if (status == google.maps.DirectionsStatus.OK){
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
 
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
@@ -36,6 +58,6 @@ function initialize(){
       markers.push(end_marker);
     });
   }
-}
+
 
 
