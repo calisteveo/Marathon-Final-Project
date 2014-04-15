c9de4f365c83d599e6d2318674010fa125601704
calisteveo <soglesbee1@gmail.com>
2014-04-15 11:23:51 -0700

    created waypoints model to enable user to change path

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327185705) do
+ActiveRecord::Schema.define(version: 20140415182134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20140327185705) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+  end
+
+  create_table "waypoints", force: true do |t|
+    t.decimal  "way_lat"
+    t.decimal  "way_lng"
+    t.integer  "journey_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
